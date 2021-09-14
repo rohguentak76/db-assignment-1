ae5c8bf256ecede4b18be7a6e725ce04ccf95979
rohguentak <rhgt78@gmail.com>
2021-09-14 17:58:56 +0900

    update code for assignment2

--- 201520860_노근탁.py
+++ 201520860_노근탁.py
@@ -5,71 +5,47 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import time
 import pyperclip
+import json
+from collections import OrderedDict
 
 LOGIN_INFO = {
-    'ID' : 'your naver id',
-    'PASSWORD' : "your naver password"
+    'ID' : 'mmnani',
+    'PASSWORD' : "Mjoh!031223@"
 }
 
 delay=3
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome(executable_path='./chromedriver',options=chrome_options)
 
-def crawler_with_login(login_url,page_url,filename):
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-dev-shm-usage')
-    driver = webdriver.Chrome(executable_path='./chromedriver',options=chrome_options)
-    driver.get(login_url)
-    driver.implicitly_wait(delay)
-    tag_id = driver.find_element_by_name('id')
-    tag_pw = driver.find_element_by_name('pw')
-    tag_id.click()
-    pyperclip.copy(LOGIN_INFO['ID'])
-    tag_id.send_keys(Keys.CONTROL, 'v')
-    time.sleep(delay)
-    tag_pw.click()
-    pyperclip.copy(LOGIN_INFO['PASSWORD']) 
-    tag_pw.send_keys(Keys.CONTROL, 'v') 
-    time.sleep(delay)
-    login_btn = driver.find_element_by_xpath('//*[@id="log.login"]')
-    login_btn.click() 
-    time.sleep(delay)
-    driver.get(page_url)
-    driver.implicitly_wait(delay + 10)
-    bsObject = BeautifulSoup(driver.page_source, "html.parser")    
-    file = open(filename,"w")
-    file.write(str(bsObject))
-    file.close()
-    print(bsObject)
-    
-
-def crawler(target_url,filename):
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-dev-shm-usage')
+def crawler(target_url):
     driver = webdriver.Chrome(executable_path='./chromedriver',options=chrome_options)
     driver.get(target_url)
-    driver.implicitly_wait(delay)
     bsObject = BeautifulSoup(driver.page_source, "html.parser")    
-    file = open(filename,"w")
-    file.write(str(bsObject))
-    file.close()
-    print(bsObject)
+    webtoonInfoDivs = bsObject.find_all("div",{"class":"challengeInfo"})
+    for webtoonInfoDiv in webtoonInfoDivs:
+        title = webtoonInfoDiv.find("a").text.strip()
+        user = webtoonInfoDiv.find("a",{"class":"user"}).text.strip()
+        summary = webtoonInfoDiv.find("div",{"class":"summary"}).text.strip()
+        star = float(webtoonInfoDiv.find("strong").text)
+        objForJson = OrderedDict()
+        objForJson["title"] = title
+        objForJson["user"] = user
+        objForJson["summary"] = summary
+        objForJson["star"] = star
+        with open(title + ".json","w",encoding="utf-8") as make_json_file:
+            json.dump(objForJson,make_json_file,ensure_ascii=False)
 
 def main():
-    page = 2
-    # target = f"https://book.naver.com/category/index.nhn?cate_code=100&tab=new_book&list_type=list&sort_type=publishday&page={page}"
-    comic_target = f"https://comic.naver.com/genre/challenge?&page={page}"
+    start_page = 3
+    end_page = 6
     comic_mypage = "https://comic.naver.com/mypage/favorite"
     nav_login = "https://nid.naver.com/nidlogin.login?mode=form&url=https%3A%2F%2Fwww.naver.com"
-    comic_filename = f"naver_new_comic_page_{page}.html"
-    comic_mypage_filename = f"naver_new_comic_mypage_page_{page}.html"
-    crawler(comic_target,comic_filename)
-    crawler_with_login(nav_login,comic_mypage,comic_mypage_filename)
-    # filename = f"naver_new_book_page_{page}.html"
-    # filename_with_login = f"naver_new_book_page_with_login_{page}.html"
-    # crawler(target,filename)
-    # crawler_with_login(nav_login,target,filename_with_login)
-
+    for page in range(start_page,end_page + 1):
+        target_url = f"https://comic.naver.com/genre/challenge?&page={page}"
+        crawler(target_url)
+    
 
 if __name__ == "__main__":
     main()
